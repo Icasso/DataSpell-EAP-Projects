2ca42b4e080893183b561861348a587f412a3b77
Icasso <isaactsui2000@gmail.com>
2022-02-03 17:46:47 +0800

    02/03: Update

---
+++ Database Related/database.py
@@ -0,0 +1,19 @@
+import psycopg2
+import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine('postgresql://@localhost:5432/Test')
+
+
+# Load dataframe
+def dataframe():
+    comments_df = pd.read_pickle("../Sentiment Analysis/comments_with_textblob_df.pkl")
+    return comments_df
+
+
+if __name__ == '__main__':
+    df = dataframe()
+    # df = df.head()
+    df.to_sql('data', engine, if_exists='replace')
+    print("Process completed")
+#%%
