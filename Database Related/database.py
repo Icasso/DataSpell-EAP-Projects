ffa874695088b2539d262957d7483983b7358891
Icasso <isaactsui2000@gmail.com>
2022-02-11 23:09:19 +0800

    02/11: Update

--- Database Related/database.py
+++ Database Related/database.py
@@ -8,13 +8,13 @@ azure_engine = create_engine('postgresql://tsuihoiming:Qw645987321@fyp-psql.post
 
 # Load dataframe
 def dataframe():
-    df = pd.read_pickle("../Pickles/list.pkl")
+    df = pd.read_pickle("../Pickles/comment_volume_filled_transposed_df.pkl")
     return df
 
 
 if __name__ == '__main__':
     res_df = dataframe()
     # df = df.head()
-    res_df.to_sql('reddit_community_distribution', azure_engine, if_exists='replace')
+    res_df.to_sql('reddit_comment_volume', azure_engine, if_exists='replace')
     print("Process completed")
 # %%
