567b05a1c71efb9acdd6699a231da9b34996b6d7
Isaac Tsui <isaactsui2000@gmail.com>
2022-03-16 20:38:27 +0800

    03/16: Update

--- Database Related/database.py
+++ Database Related/database.py
@@ -2,19 +2,20 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 engine = create_engine('postgresql://@localhost:5432/Test')
-azure_engine = create_engine('postgresql://tsuihoiming:Qw645987321@fyp-psql.postgres.database.azure.com/Development'
-                             '?sslmode=require')
+azure_engine = create_engine(
+    'postgresql://tsuihoiming:r*x93Rd6ZVxXgccpwgvyr&4mXRo!Pb@fyp-psql.postgres.database.azure.com/Development'
+    '?sslmode=require')
 
 
 # Load dataframe
 def dataframe():
-    df = pd.read_pickle("../Pickles/temp/out_symbol_mvader_compound.pkl")
+    df = pd.read_pickle("/Users/isaac/DataSpell-EAP Projects/Reddit Data/term_document_matrix.pkl")
     return df
 
 
 if __name__ == '__main__':
     res_df = dataframe()
     # df = df.head()
-    res_df.to_sql('sa_modified_vader_compound', engine, if_exists='replace')
+    res_df.to_sql('term_document_matrix', azure_engine, if_exists='replace')
     print("Process completed")
 # %%
